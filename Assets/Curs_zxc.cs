ef725f078bc581c5e2e00901b184dae0318b4ed9
DmitryShetinin <shchetinin-02@list.ru>
2021-05-19 09:39:24 +0300

    map

---
+++ Assets/Curs_zxc.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curs_zxc : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
